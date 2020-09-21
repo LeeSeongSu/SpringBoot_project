8601dca97ac51430d963f1c3d2c04cca6a5d9f95
LeeSeongSu <oplm123456@naver.com>
2020-09-21 15:03:01 +0900

    login first layout

--- src/main/java/com/victolee/board/config/WebSecurityConfig.java
+++ src/main/java/com/victolee/board/config/WebSecurityConfig.java
@@ -33,9 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
     protected void configure(HttpSecurity http) throws Exception { // 6
         http
                 .authorizeRequests() // 접근에 대한 인증 설정
-                .antMatchers("/","/login","/signup","/user").permitAll() // 누구나 접근 허용
-                .antMatchers("/post").hasRole("TRAINER") // USER, ADMIN만 접근 가능
-//                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .antMatchers("/login","/signup","/user").permitAll() // 누구나 접근 허용
+                .antMatchers("/post").hasRole("TRAINER") // Trainer만 접근 가능
+//                .antMatchers("/admin").hasRole("ADMIN") //
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
                 .formLogin() // 로그인에 관한 설정
